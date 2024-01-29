09e192a6ad72771de7cddfbf810b71a920b85b47
unknown <chayansharma77@gmail.com>
2024-01-29 23:15:50 +0530

    pattern

---
+++ FactoryPattern/Main.java
@@ -0,0 +1,11 @@
+package FactoryPattern;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        ShapeFactory shapeFactoryObj = new ShapeFactory();
+        Shape shapeObj = shapeFactoryObj.getShape("CIRCLE");
+        shapeObj.draw();
+    }
+}
